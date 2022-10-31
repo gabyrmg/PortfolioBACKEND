62c4f8f6dd986844e9561f776e60747da7972cff
gabyrmg <100819516+gabyrmg@users.noreply.github.com>
2022-10-31 11:12:02 -0300

    tratando de arreglar conexion

--- GRMG/src/main/java/com/portfolio/GRMG/Controller/CPersona.java
+++ GRMG/src/main/java/com/portfolio/GRMG/Controller/CPersona.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("persona")
+@RequestMapping("/persona")
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://grmgfront.web.app")
 public class CPersona {
